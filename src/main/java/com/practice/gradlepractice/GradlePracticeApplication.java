88c4dc39066ee78c14bd276783736e795b94a50a
parameswaranvv <parameswaranvv@gmail.com>
2018-11-12 10:11:15 -0600

    Initial Commit

---
+++ src/main/java/com/practice/gradlepractice/GradlePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.gradlepractice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class GradlePracticeApplication {
+
+    public static void main(String[] args) {
+        run(GradlePracticeApplication.class, args);
+    }
+}
